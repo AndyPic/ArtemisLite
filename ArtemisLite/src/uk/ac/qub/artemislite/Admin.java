f1f8864a345a623b38fdd80cb01a1a9585f12a5e
Jordan <jdavis15@qub.ac.uk>
2021-02-18 14:46:58 +0000

    Initial Project Admin Class Setup

---
+++ ArtemisLite/src/uk/ac/qub/artemislite/Admin.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.qub.artemislite;
+
+/**
+ * @author Jordan Davis
+ *
+ */
+public class Admin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
