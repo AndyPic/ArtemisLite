168f379351548ff51bb05fb69f4594f4f9114b7c
Jordan <jdavis15@qub.ac.uk>
2021-02-19 16:24:43 +0000

    Resource Square created

---
+++ ArtemisLite/src/uk/ac/qub/artemislite/ResourceSquare.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package uk.ac.qub.artemislite;
+
+/**
+ * @author Jordan Davis
+ *
+ */
+public class ResourceSquare extends Square {
+
+	/**
+	 * 
+	 */
+	private final int RESOURCES_TO_ALLOCATE = 200;
+
+	/**
+	 * default constructor
+	 */
+	public ResourceSquare() {
+
+	}
+
+	/**
+	 * Constructor with args
+	 * @param boardPosition
+	 * @param squareName
+	 * @param resourcesToAllocate
+	 */
+	public ResourceSquare(int boardPosition, String squareName) {
+		super(boardPosition, squareName);
+	}
+
+	/**
+	 * Increase player resources
+	 * @param player
+	 */
+	public void giveInvestment(Player player) {
+		
+		int currentResource;
+		currentResource = player.getBalanceOfResources();
+
+		player.setBalanceOfResources(currentResource + this.RESOURCES_TO_ALLOCATE);
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return "ResourceSquare [resourcesToAllocate=" + this.RESOURCES_TO_ALLOCATE + "]";
+	}
+
+}
