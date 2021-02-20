321afca9330c274e1e4a716b8b8e36c040f533d5
Joseph <40145951@ads.qub.ac.uk>
2021-02-20 14:23:05 +0000

    Testing eclipse integration JSM

---
+++ Testing/src/test_JSM/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test_JSM;
+
+/**
+ * @author Joseph Mawhinney
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Joseph Mawhinney.");
+
+	}
+
+}
