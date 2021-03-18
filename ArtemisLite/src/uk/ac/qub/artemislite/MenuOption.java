6c1447bd632bc527c3843eabbf3f3d3912d0df21
Jordan <jdavis15@qub.ac.uk>
2021-03-18 17:46:20 +0000

    MenuOprion Enum created

---
+++ ArtemisLite/src/uk/ac/qub/artemislite/MenuOption.java
@@ -0,0 +1,35 @@
+package uk.ac.qub.artemislite;
+
+/**
+ * Possible Menu Options
+ * @author Jordan Davis
+ *
+ */
+public enum MenuOption {
+
+	VIEW_ALL_ELEMENTS ("View all owned elements"),
+	VIEW_PLAYER_ELEMENTS ("View elements you own"),
+	GET_SQUARE_DETAILS ("Get current element details"),
+	INCREASE_DEVELOPMENT ("Increase element development"),
+	END_TURN ("End your turn"),
+	END_GAME ("End the game");
+	
+	private final String menuOption;
+	
+	/**
+	 * 
+	 * @param menuOption
+	 */
+	private MenuOption(String menuOption) {
+		this.menuOption = menuOption;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getMenuOption() {
+		return this.menuOption;
+	}
+	
+}
